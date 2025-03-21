e5de4bc016496feb3fc66d43343dac60789ffdef
Renn Akaza <8340896+AkazaRenn@users.noreply.github.com>
2025-03-21 17:37:27 +0000

    Handle playwright update

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 CONTAINER_NAME="steam-game-remover"
+TAG=$(curl -s https://mcr.microsoft.com/v2/playwright/python/tags/list | jq -r '.tags | .[]' | sort -rV | grep jammy-amd64 | head -n 1)
+IMAGE="mcr.microsoft.com/playwright/python:${TAG}"
 
+docker pull ${IMAGE}
 docker rm -f ${CONTAINER_NAME} &> /dev/null
-docker run -d --name ${CONTAINER_NAME} -v $(pwd):$(pwd) -w $(pwd) --entrypoint $(pwd)/entrypoint.sh mcr.microsoft.com/playwright/python:v1.50.0-jammy
+docker run -d --name ${CONTAINER_NAME} --restart unless-stopped -v $(pwd):$(pwd) -w $(pwd) --entrypoint $(pwd)/entrypoint.sh ${IMAGE}
